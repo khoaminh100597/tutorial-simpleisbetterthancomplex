dd3a349c248ae0ed3eb731c2faa043f6458e0abe
khoaminh100597 <khoaminh.energy@gmail.com>
2019-04-17 00:05:27 +0700

    Second commit

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/khoaminh100597/Desktop/My life/Django/tutorial-simpleisbetterthancomplex/venv/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
